f7196ed887341a66a2d0009c4e68181f5e9c7d1a
rummur1204 <rummur1204@gmail.com>
2025-10-23 23:11:25 +0300

    Initial commit

---
+++ database/migrations/2025_10_23_164603_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['Pending', 'In Progress', 'Completed'])->default('Pending');
+            $table->date('due_date')->nullable();
+            $table->integer('progress')->default(0);
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
